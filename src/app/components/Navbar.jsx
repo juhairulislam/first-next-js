cd80420edc5c284ddbf0745aa5b03a97ee8a92e1
juhairulislam <juhairulislam2018@gmail.com>
2026-04-09 15:07:59 +0600

    creating multiple layout in next js

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -8,6 +8,7 @@ const Navbar = () => {
             <li ><Link href='/about'>About</Link></li>
             <li ><Link href='/contact'>Contact</Link></li>
             <li ><Link href='/blogs'>Blogs</Link></li>
+            <li ><Link href='/dashboard'>Dashboard</Link></li>
 
     
     </>
---
+++ src/app/dashboard/profile/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DashboardProfile = () => {
+    return (
+        <div>
+            <h1>This is profile re bhai</h1>
+        </div>
+    );
+};
+
+export default DashboardProfile;
